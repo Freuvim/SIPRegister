1f3f30b0260bfffa9d4c047279e76ef6e7de449a
Kélvin Tesche Ievenes <kelvin_tesche@hotmail.com>
2017-08-29 10:05:49 -0300

    Boot in Android O

--- app/src/main/java/io/github/freuvim/sipregister/receivers/BroadcastReceiverOnBootCompleted.java
+++ app/src/main/java/io/github/freuvim/sipregister/receivers/BroadcastReceiverOnBootCompleted.java
@@ -3,15 +3,18 @@ package io.github.freuvim.sipregister.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
+import android.os.Build;
 
 import io.github.freuvim.sipregister.services.BackgroundService;
 
 public class BroadcastReceiverOnBootCompleted extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        context.registerReceiver(this, new IntentFilter(intent.getAction()));
         Intent serviceIntent = new Intent(context, BackgroundService.class);
-        context.stopService(serviceIntent);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(serviceIntent);
+        } else {
+            context.startService(serviceIntent);
+        }
     }
 }
